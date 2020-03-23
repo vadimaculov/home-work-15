f324cc679153624603cdd026bf46c2ca0d503aa9
Vadim Aculov <vadim.aculov@gmail.com>
2020-03-23 18:57:31 +0200

    HW15-Finish homework-15

--- main.js
+++ main.js
@@ -1,17 +1,34 @@
+// ==================== HomeWork-15 ==========================
+
+// function showFirst() {
+//     document.querySelector('#pr').innerHTML = 'You clicked First button';
+// }
+
+// function showSecond() {
+//     document.querySelector('#pr').innerHTML = 'You clicked Second button';
+// }
+
+// function showThird() {
+//     document.querySelector('#pr').innerHTML = 'You clicked Third button';
+// }
+
+// document.getElementById('second').onclick = showSecond;
+
+// document.querySelector('.third').addEventListener('click', showThird);
+
+// ==================== HomeWork-16 ==========================
 function showFirst() {
-    // alert('You clicked First button')
-    document.querySelector('#pr h4').innerHTML = 'You clicked First button';
+    document.querySelector('#pr').innerHTML = 'You clicked First button';
 }
 
 function showSecond() {
-    // alert('You clicked Second button')
-    document.querySelector('#pr h4').innerHTML = 'You clicked Second button';
+    document.querySelector('#pr').innerHTML = 'You clicked Second button';
 }
 
 function showThird() {
-    alert('You clicked Third button')
+    document.querySelector('#pr').innerHTML = 'You clicked Third button';
 }
 
 document.getElementById('second').onclick = showSecond;
 
-document.querySelector('.third').addEventListener('click', showThird)
+document.querySelector('.third').addEventListener('click', showThird);
